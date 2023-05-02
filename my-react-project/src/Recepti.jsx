30cdd95b5431704628aa141af734909b65f1091e
marija141414 <marijaorlic14@gmail.com>
2023-05-02 17:28:45 +0200

    dodati filteri na komponentu recepti

--- my-react-project/src/Recepti.jsx
+++ my-react-project/src/Recepti.jsx
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
- 
 
 const Recepti = () => {
   const [recepti, setRecepti] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedArea, setSelectedArea] = useState("");
+  const [categories, setCategories] = useState([]);
+  const [areas, setAreas] = useState([]);
 
   useEffect(() => {
     const ucitajRecepte = async () => {
       const response = await axios.get(
-        "https://www.themealdb.com/api/json/v1/1/search.php?s=chicken"
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`
       );
 
       if (response.data && response.data.meals) {
@@ -17,25 +21,101 @@ const Recepti = () => {
     };
 
     ucitajRecepte();
+  }, [searchTerm]);
+
+  useEffect(() => {
+    const ucitajKategorije = async () => {
+      const response = await axios.get(
+        "https://www.themealdb.com/api/json/v1/1/categories.php"
+      );
+
+      if (response.data && response.data.categories) {
+        setCategories(response.data.categories);
+      }
+    };
+
+    ucitajKategorije();
   }, []);
 
+  useEffect(() => {
+    const ucitajPodrucja = async () => {
+      const response = await axios.get(
+        "https://www.themealdb.com/api/json/v1/1/list.php?a=list"
+      );
+
+      if (response.data && response.data.meals) {
+        setAreas(response.data.meals);
+      }
+    };
+
+    ucitajPodrucja();
+  }, []);
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const handleAreaChange = (event) => {
+    setSelectedArea(event.target.value);
+  };
+
+  const filteredRecepti = recepti.filter((recept) => {
+    const categoryMatch = !selectedCategory || recept.strCategory === selectedCategory;
+    const areaMatch = !selectedArea || recept.strArea === selectedArea;
+
+    return categoryMatch && areaMatch;
+  });
+
   return (
-    <div className="recepti-container">
-      {recepti.map((recept) => (
-        <div className="recept-kartica" key={recept.idMeal}>
-          <img
-            className="recept-slika"
-            src={recept.strMealThumb}
-            alt={recept.strMeal}
-          />
-          <div className="recept-tekst">
+    <div>
+    
+            <div className="recepti-container">
+            <input
+        type="text"
+        value={searchTerm}
+        onChange={handleSearchChange}
+        placeholder="Pretraži recepte"
+      />
+      <select value={selectedCategory} onChange={handleCategoryChange}>
+        <option value="">Izaberi kategoriju</ option>
+            {categories.map((category) => (
+            <option key={category.idCategory} value={category.strCategory}>
+            {category.strCategory}
+            </option>
+            ))}
+            </select>
+            <select value={selectedArea} onChange={handleAreaChange}>
+            <option value="">Izaberi poreklo</option>
+            {areas.map((area) => (
+            <option key={area.id} value={area.strArea}>
+            {area.strArea}
+            </option>
+            ))}
+            </select>
+            {filteredRecepti.map((recept) => (
+            <div className="recept-kartica" key={recept.idMeal}>
+            <img
+                    className="recept-slika"
+                    src={recept.strMealThumb}
+                    alt={recept.strMeal}
+                    />
+            <div className="recept-tekst">
             <h3>{recept.strMeal}</h3>
             <p>{recept.strInstructions.substring(0, 200)}...</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+            <p>
+            Kategorija: {recept.strCategory || "N/A"} | Poreklo:{" "}
+            {recept.strArea || "N/A"}
+            </p>
+            </div>
+            </div>
+            ))}
+            </div>
+            </div>
+            );
 };
 
 export default Recepti;
